d3659242568c9e055532270ec379bf05e87a1a5f
Your Naere <naere.loire@gmailcom>
2021-05-28 17:12:50 -0300

    add subcategory useCase

--- src/main/java/br/com/training/cleanarch/usecase/FindSubcategory.java
+++ src/main/java/br/com/training/cleanarch/usecase/FindSubcategory.java
@@ -1,4 +1,14 @@
 package br.com.training.cleanarch.usecase;
 
+import br.com.training.cleanarch.domain.SubcategoryEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FindSubcategory {
+
+    private SubcategoryGateway subcategoryGateway;
+
+    public FindSubcategory(SubcategoryGateway subcategoryGateway){ this.subcategoryGateway = subcategoryGateway; }
+
+    public List <SubcategoryEntity> findAllSubcategories (){return subcategoryGateway.findAll();}
 }
